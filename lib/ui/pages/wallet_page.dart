5686f958089c8e8c3deb3524aed8bf21703efaa0
SofianIndraM <opign101@gmail.com>
2023-01-31 10:29:21 +0700

    Second Commit

--- lib/ui/pages/wallet_page.dart
+++ lib/ui/pages/wallet_page.dart
@@ -12,80 +12,82 @@ class WalletPage extends StatelessWidget {
     return BlocBuilder<AuthCubit, AuthState>(
       builder: (context, state) {
         if (state is AuthSuccess) {
-          return Container(
-            padding: EdgeInsets.all(defaultMargin),
-            height: 211,
-            width: 300,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage(
-                  'assets/image_card.png',
+          return Center(
+            child: Container(
+              padding: EdgeInsets.all(defaultMargin),
+              height: 211,
+              width: 300,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                    'assets/image_card.png',
+                  ),
                 ),
+                boxShadow: [
+                  BoxShadow(
+                    color: kPrimaryColor.withOpacity(0.5),
+                    blurRadius: 50,
+                    offset: Offset(0, 10),
+                  ),
+                ],
               ),
-              boxShadow: [
-                BoxShadow(
-                  color: kPrimaryColor.withOpacity(0.5),
-                  blurRadius: 50,
-                  offset: Offset(0, 10),
-                ),
-              ],
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  children: [
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Name',
-                            style: whiteTextStyle.copyWith(
-                              fontWeight: light,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Name',
+                              style: whiteTextStyle.copyWith(
+                                fontWeight: light,
+                              ),
                             ),
-                          ),
-                          Text(
-                            '${state.user.name}',
-                            style: whiteTextStyle.copyWith(
-                              fontSize: 20,
-                              fontWeight: medium,
+                            Text(
+                              '${state.user.name}',
+                              style: whiteTextStyle.copyWith(
+                                fontSize: 20,
+                                fontWeight: medium,
+                              ),
+                              overflow: TextOverflow.ellipsis,
                             ),
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
-                    ),
-                    Container(
-                      width: 24,
-                      height: 24,
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: AssetImage(
-                            'assets/icon_plane.png',
+                      Container(
+                        width: 24,
+                        height: 24,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage(
+                              'assets/icon_plane.png',
+                            ),
                           ),
                         ),
                       ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 41,
+                  ),
+                  Text(
+                    'Balance',
+                    style: whiteTextStyle.copyWith(
+                      fontWeight: light,
                     ),
-                  ],
-                ),
-                SizedBox(
-                  height: 41,
-                ),
-                Text(
-                  'Balance',
-                  style: whiteTextStyle.copyWith(
-                    fontWeight: light,
                   ),
-                ),
-                Text(
-                  'IDR 280.000.000',
-                  style: whiteTextStyle.copyWith(
-                    fontSize: 26,
-                    fontWeight: medium,
+                  Text(
+                    'IDR 280.000.000',
+                    style: whiteTextStyle.copyWith(
+                      fontSize: 26,
+                      fontWeight: medium,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           );
         } else {
